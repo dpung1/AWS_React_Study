dec41a03d612045b36dd895db92e0a601c86acf5
dpung1 <pung_12@naver.com>
2023-08-17 20:02:40 +0900

    [AWS] 산대특 React 수업 (23.08.17) - css 수정

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -4,14 +4,15 @@ import React, { useRef, useState } from 'react';
 import * as S from './Style.js';
 
 function Mypage(props) {
+    // 초기 이미지 상태
     const defaultProfileImgSrc = "https://lippianfamilydentistry.net/wp-content/uploads/2015/11/user-default.png";
-
+    // 로컬 스토리지에서 이미지 정보 가져오기
     const profileImgSrc = localStorage.getItem("profileImg");
-    
+    // 프로필 객체 생성 (이미지가 있으면 profileImgSrc, 없으면 defaultProfileImgSrc)
     const [ profileImg, setProfileImg ] = useState(!!profileImgSrc ? profileImgSrc : defaultProfileImgSrc)
-
+    // 로컬 스토리지에서 유저 정보 가져오기
     const localStorageUser = JSON.parse(localStorage.getItem("user"));
-
+    // 사용자 객체 생성
     let user = {
         username: localStorageUser?.username && localStorageUser.username,
         name: localStorageUser?.name && localStorageUser.name,
@@ -19,11 +20,11 @@ function Mypage(props) {
     }
 
     const profileFileInput = useRef();
-
+    // 이미지 변경 클릭 이벤트 
     const handleProfileImgChangeClick = () => {
         profileFileInput.current.click();
     }
-    
+    // 이미지 선택 이벤트
     const handleProfileImgSelect = (e) => {
         const selectedImg = e.target.files[0];
 
@@ -34,10 +35,11 @@ function Mypage(props) {
                 setProfileImg(e.target.result);
             }
     
-            reader.readAsDataURL(e.target.files[0]);
+            reader.readAsDataURL(selectedImg);
         }
-    }
 
+    }
+    // 유저 정보 변경 이벤트 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         user = {
@@ -45,35 +47,35 @@ function Mypage(props) {
             [name]: value
         }
     }
-
+    // 로컬 스토리지 저장 클릭 이벤트 
     const handleSaveClick = () => {
         localStorage.setItem("user", JSON.stringify(user));
-        localStorage.setItem("profileImg", JSON.stringify(profileImg));
+        localStorage.setItem("profileImg", profileImg);
     }
-
+     // HTML 관련 코드 
     return (
         <div css={S.SLayout}>
             <div css={S.SContainer}>
                 <div css={S.SProfileImgBox} onClick={handleProfileImgChangeClick}>
                     <img css={S.SProfileImg} src={profileImg} />
-                    <input css={S.SFileHidden}type="file" ref={profileFileInput} onChange={handleProfileImgSelect}/>
+                    <input css={S.SFileHidden} type="file" ref={profileFileInput} onChange={handleProfileImgSelect}/>
                 </div>
             </div>
             <div css={S.SContainer}>
-                <div>
-                    <label htmlFor="username">사용자명 </label>
-                    <input type="text" id='username' name="username" onChange={handleInputChange} defaultValue={user.username}/>
+                <div css={S.SUserInfo}>
+                    <label css={S.SLabel} htmlFor="username">사용자명 </label>
+                    <input css={S.SInput} type="text" id='username' name="username" onChange={handleInputChange} defaultValue={user.username}/>
+                </div>
+                <div css={S.SUserInfo}>
+                    <label css={S.SLabel} htmlFor="name">이름 </label>
+                    <input css={S.SInput} type="text" id='name' name="name" onChange={handleInputChange} defaultValue={user.name}/>
+                </div>
+                <div css={S.SUserInfo}>
+                    <label css={S.SLabel} htmlFor="email">이메일 </label>
+                    <input css={S.SInput} type="text" id='email' name="email" onChange={handleInputChange} defaultValue={user.email}/>
                 </div>
                 <div>
-                    <label htmlFor="name">이름 </label>
-                    <input type="text" id='name' name="name" onChange={handleInputChange} defaultValue={user.name}/>
-                </div>
-                <div>
-                    <label htmlFor="email">이메일 </label>
-                    <input type="text" id='email' name="email" onChange={handleInputChange} defaultValue={user.email}/>
-                </div>
-                <div>
-                    <button onClick={handleSaveClick}>저장</button>
+                    <button css={S.SButton}onClick={handleSaveClick}>저장</button>
                 </div>
             </div>
         </div>
--- src/pages/Mypage/Style.js
+++ src/pages/Mypage/Style.js
@@ -7,10 +7,12 @@ export const SLayout = css`
     align-items: center;
 
     margin: 50px auto;
-    border: 1px solid #dbdbdb;
+    border: 1px solid #D2DAFF;
+    border-radius: 25px;
     padding: 20px;
     width: 500px;
-    height: 500px;
+    height: 750px;
+    background-color: #B1B2FF;
 `;
 
 export const SContainer = css`
@@ -27,7 +29,7 @@ export const SProfileImgBox = css`
     justify-content: center;
     align-items: center;
 
-    border: 1px solid #dbdbdb;
+    border: 1px solid #D2DAFF;
     border-radius: 50%;
     width: 300px;
     height: 300px;
@@ -41,4 +43,32 @@ export const SProfileImg = css`
 
 export const SFileHidden = css`
     display: none;
+`;
+
+export const SUserInfo = css`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const SLabel = css`
+    text-align: center;
+
+    margin: 1px;
+`;
+
+export const SInput = css`
+    margin: 3px;
+    padding: 3px;
+    border: 1px solid #D2DAFF;
+    background-color: #EEF1FF;
+`;
+
+export const SButton = css`
+    margin: 10px;
+    border: 1px solid #D2DAFF;
+    border-radius: 5px;
+    padding: 5px;
+    width: 300px;
+    background-color: #AAC4FF;
+    cursor: pointer;
 `;
